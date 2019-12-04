012f1a3a0901ca82728c7b697b3e185be03e0a7f
chazzam <chazzam@users.noreply.github.com>
2019-12-03 22:18:24 -0600

    Got Day 3 part 1 working!
    Need to make sure neither initial point is 0,0, not just the first
    when identifying intersections.

--- src/main.rs
+++ src/main.rs
@@ -137,104 +137,138 @@ struct Point {
 struct Line (i32, i32, i32, i32);
 
 fn line_intersection(one: &Line, two: &Line) ->
-      Option<(i32, i32)> {
-  if one.0 == 0 && one.1 == 0 {
+    Option<(i32, i32)> {
+  if one.0 == 0 && one.1 == 0 && two.0 == 0 && two.1 == 0 {
     return None;
   }
 
-  let s1: Point = Point {
-      x:one.2 - one.0,
-      y:one.3 - one.1};
-  let s2: Point = Point {
-      x:two.2 - two.0,
-      y:two.3 - two.1};
+  let p0 = Point { x:one.0, y:one.1};
+  let p1 = Point { x:one.2, y:one.3};
+  let p2 = Point { x:two.0, y:two.1};
+  let p3 = Point { x:two.2, y:two.3};
 
-  let d:i32 = -s2.x * s1.y + s1.x * s2.y;
+  let s1 = Point {
+      x:p1.x - p0.x,
+      y:p1.y - p0.y};
+  let s2 = Point {
+      x:p3.x - p2.x,
+      y:p3.y - p2.y};
+
+  //float s, t;
+  let d:i32 = s1.x * s2.y - s2.x * s1.y;
   if d == 0 {
     return None;
   }
+  let d_pos = d > 0;
 
-  let s:i32 = (-s1.y * (one.0 - two.0) + s1.x * (one.1 - two.1)) / d;
-  let t:i32 = ( s2.x * (one.1 - two.1) - s2.y * (one.0 - two.0)) / d;
-
-  if s >= 0 && s <= 1 && t >= 0 && t <= 1 {
-    // Collision detected
-    return Some(
-      (one.0 + (t * s1.x),
-       one.1 + (t * s1.y))
-    );
+  let s02 = Point {
+    x: p0.x - p2.x,
+    y: p0.y - p2.y,
+  };
+  let s_numerator = s1.x * s02.y - s1.y * s02.x;
+  if (s_numerator < 0) == d_pos {
+    return None;
   }
 
-  None
+  let t_numerator = s2.x * s02.y - s2.y * s02.x;
+  if (t_numerator < 0) == d_pos {
+    return None;
+  }
+
+  if (s_numerator > d) == d_pos || (t_numerator > d) == d_pos {
+    return None;
+  }
+
+  let t:f64 = f64::from(t_numerator) / f64::from(d);
+
+  // Collision detected
+  return Some(
+    (p0.x + (t * f64::from(s1.x)) as i32,
+     p0.y + (t * f64::from(s1.y)) as i32)
+  );
 }
 
 fn day_three() {
-  //let raw_input = include_str!("d3_1.lst");
-  let raw_input = "R8,U5,L5,D3\nU7,R6,D4,L4\n";
-  //let raw_input = "R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83\n";
-  let mut raw_lines = raw_input.lines();
+  let manhattanize = |input: &str| {
+    let mut raw_lines = input.lines();
 
-  let directionalize = |x: &str| {
-    let val: i32 = x[1..]
-        .trim()
-        .parse()
-        .unwrap();
-    let mut z = Point { x:0, y:0};
-    match x.chars().nth(0).unwrap() {
-      'R' => z.x =  val,
-      'L' => z.x = -val,
-      'U' => z.y =  val,
-       _  => z.y = -val,
+    let directionalize = |x: &str| {
+      let val: i32 = x[1..]
+          .trim()
+          .parse()
+          .unwrap();
+      let mut z = Point { x:0, y:0};
+      match x.chars().nth(0).unwrap() {
+        'R' => z.x =  val,
+        'L' => z.x = -val,
+        'U' => z.y =  val,
+        _  => z.y = -val,
+      }
+      z
+    };
+
+    let linify = |raw: &String| {
+      let mut line: Vec<Line> = Vec::new();
+      let mut last = Point { x:0, y:0};
+      for p in raw.split(',').map(|x| directionalize(x)) {
+        line.push(Line (last.x, last.y, last.x + p.x, last.y + p.y));
+        last.x += p.x;
+        last.y += p.y;
+      }
+      line
+    };
+
+    let line1 = linify(&raw_lines
+          .next()
+          .unwrap()
+          .to_string());
+    let line2 = linify(&raw_lines
+          .next()
+          .unwrap()
+          .to_string());
+    /* print the line lists
+    println!("{}", line1
+        .iter()
+        .fold(String::new(), |acc, x| acc + &format!(" ({}, {}, {}, {});", x.0, x.1, x.2, x.3).to_string()));
+    println!("{}", line2
+        .iter()
+        .fold(String::new(), |acc, x| acc + &format!(" ({}, {}, {}, {});", x.0, x.1, x.2, x.3).to_string()));
+    */
+
+    /* Get the full intersections + distance list
+    let mut intersects: std::collections::HashSet<(i32,i32)> = std::collections::HashSet::new();
+    for i in line1.iter() {
+      intersects.extend(
+        line2.iter()
+        .filter_map(|x| line_intersection(i, x)));
     }
-    z
+
+    println!("intersects: [");
+    for i in intersects.iter() {
+      print!(" ({},{})->{},", i.0, i.1, i.0.abs()+i.1.abs());
+    }
+    println!("\n]");
+    */
+
+    let mut distances: Vec<i32> = Vec::new();
+    for i in line1.iter() {
+      distances.extend(
+        line2.iter()
+        .filter_map(|x| line_intersection(i, x))
+        .map(|(x,y)| x.abs() + y.abs()));
+    }
+    distances.sort();
+    return distances;
   };
 
-  let linify = |raw: &String| {
-    let mut line: Vec<Line> = Vec::new();
-    let mut last = Point { x:0, y:0};
-    for p in raw.split(',').map(|x| directionalize(x)) {
-      line.push(Line (last.x, last.y, last.x + p.x, last.y + p.y));
-      last.x += p.x;
-      last.y += p.y;
-    }
-    line
-  };
+  println!("D3p1::Test 1:   6 ?= {: >4}", manhattanize("R8,U5,L5,D3\nU7,R6,D4,L4\n")[0]);
+  println!("D3p1::Test 2: 159 ?= {: >4}", manhattanize("R75,D30,R83,U83,L12,D49,R71,U7,L72\nU62,R66,U55,R34,D71,R55,D58,R83\n")[0]);
+  println!("D3p1::Test 3: 135 ?= {: >4}", manhattanize("R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51\nU98,R91,D20,R16,D67,R40,U7,R15,U6,R7\n")[0]);
 
-  let line1 = linify(&raw_lines
-        .next()
-        .unwrap()
-        .to_string());
-  let line2 = linify(&raw_lines
-        .next()
-        .unwrap()
-        .to_string());
-  println!("{}", line1
-      .iter()
-      .fold(String::new(), |acc, x| acc + &format!(" ({}, {}, {}, {});", x.0, x.1, x.2, x.3).to_string()));
-  println!("{}", line2
-      .iter()
-      .fold(String::new(), |acc, x| acc + &format!(" ({}, {}, {}, {});", x.0, x.1, x.2, x.3).to_string()));
+  let raw_input = include_str!("d3_1.lst"); // 554 too high
+  let intersects = manhattanize(raw_input);
 
-
-  let mut intersects: std::collections::HashSet<(i32,i32)> = std::collections::HashSet::new();
-  //let mut intersects: Vec<(i32,i32)> = Vec::new();
-  for i in line1[2..].iter() {
-    intersects.extend(
-      line2.iter()
-      .filter_map(|x| line_intersection(i, x)));
-  }
-  /*let intersects: std::collections::HashSet<(i32, i32)> =
-    line1
-      .iter()
-      .zip(line2.iter())
-      .filter_map(|(x, y)| line_intersection(x, y))
-      .collect();*/
-  println!("intersects: {}", intersects.len());
-  print!("[");
-  for i in intersects.iter() {
-    print!(" ({},{}),", i.0, i.1);
-  }
-  println!("]");
+  println!("D3p1:: {} intersections; distance: {}", intersects.len(), intersects[0]);
 }
 
 fn main() {
